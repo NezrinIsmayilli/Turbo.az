554cad4335fc779ee4d0ba089bd4dd218d5b57f1
qurbanovaayten <86964069+qurbanovaayten@users.noreply.github.com>
2023-03-26 20:03:46 +0400

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import Ehtiyat from './Ehtiyat'
+import Avtosalon from './Avtosalon'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav>
-      Nav
-      <NavLink to="/wishlist">wishlist</NavLink>
-    </nav> 
+    <div className='navbar'>
+        <h3>TURBO.AZ</h3>
+        <NavLink to="/about">Bütün elanlar</NavLink>
+        <NavLink to="/avto">Avtosalonlar</NavLink>
+        <NavLink to="/ehtiyat" className="nav-item dropdown">
+          <div className='drop'>
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Ehtiyat hissələri və aksesuarlar
+              </a>
+             
+              </div>    
+            </NavLink>
+            <NavLink to="/about">
+              <button>yeni elan</button>
+            </NavLink>
+    </div>
   )
 }
 
-export default Navbar;
+export default Navbar
